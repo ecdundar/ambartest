58050c9a023b6d78cf5e8874ff72956774338164
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-03-22 09:48:43 +0300

    SplashScreen

--- lib/screens/SplashScreen.dart
+++ lib/screens/SplashScreen.dart
@@ -10,6 +10,15 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacementNamed(context, '/Main');
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(color: ColorHelper.getBurulasMavi());
   }
